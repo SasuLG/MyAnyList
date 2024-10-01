97be54425af75570076e6c3648fc679afdae0d7f
Graziani Leo <leo.graziani@etu.u-bordeaux.fr>
2024-10-01 21:24:02 +0200

    ras le bol

--- src/app/api/user/auth/register/route.ts
+++ src/app/api/user/auth/register/route.ts
@@ -18,9 +18,9 @@ export async function POST(req: Request): Promise<Response> {
     try {
         const requestBody = await req.json();
         const { login, password, email } = requestBody;
-
+        console.log("logn,"+login, password, email);
         if (login.trim() === '' || password.trim() === '' || email.trim() === '') { return WrongCredentials(); }
-
+        console.log("trim");
         const foundUserMail: User | undefined = await getUserByMail(email);
         if (foundUserMail) {
             return new Response(JSON.stringify({ message: 'Cet email est déjà utilisé !', valid: false }), { status: 401 });
@@ -30,19 +30,21 @@ export async function POST(req: Request): Promise<Response> {
             return new Response(JSON.stringify({ message: 'Ce login est déjà utilisé !', valid: false }), { status: 401 });
         }
 
+        console.log("found");
         // Générer un token JWT
         const emailVerificationToken = jwt.sign(
             { login }, 
             process.env.JWT_SECRET ?? "default", 
             { expiresIn: '24h' } // Le token expire dans 24 heures
         );
-
+        console.log("token");
         // Envoyer l'email avec le lien de confirmation
         const verificationUrl = `${process.env.MODE === "production" ? process.env.NEXT_PUBLIC_BASE_URL_PROD: process.env.NEXT_PUBLIC_BASE_URL_DEV}/verify-email?token=${emailVerificationToken}`;
+        console.log("url");
         await sendEmail(email, 'Vérifiez votre adresse email', `${verificationUrl}`);
-        
+        console.log("send");
         await createUser(login, password, email, emailVerificationToken);
-
+        console.log("create");
         return new Response(JSON.stringify({ message: 'Inscription réussie ! Veuillez vérifier votre email', valid: true }), { status: 200 });
     } catch (err) {
         return ServerError('/api/user/auth/register', err);
